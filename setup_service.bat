73138e476e2f0f0e957fb5df4a342bacd462ddee
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-09-04 19:13:39 +0530

    service setup batch file

---
+++ setup_service.bat
@@ -0,0 +1,6 @@
+
+call nssm.exe install mis_pair_angles_service "%cd%\run_server.bat"
+call nssm.exe set mis_pair_angles_service AppStdout "%cd%\logs\mis_pair_angles_service.log"
+call nssm.exe set mis_pair_angles_service AppStderr "%cd%\logs\mis_pair_angles_service.log"
+call sc start mis_pair_angles_service
+rem call nssm.exe edit mis_dashboard
